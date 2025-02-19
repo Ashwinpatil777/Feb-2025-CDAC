debeb3227c2ec21cd81feb54aa80f475db065e1e
Ashwin patil <patil70ap@gmail.com>
2025-02-19 11:49:45 +0530

    Delete DAY_1 directory

--- DAY_1/Area_of_a_circle.java
+++
@@ -1,7 +0,0 @@
-class Area_of_a_circle {
-    public static void main(String[] args) {
-        double radius = 7;
-        double area = Math.PI * radius * radius;
-        System.out.printf("Area of the circle: %.5f\n", area);
-    }
-}
--- DAY_1/Divide_two_number.java
+++
@@ -1,8 +0,0 @@
-class Divide_two_number{
-       public static void main(String args[]){
-       int a=50;
-       int b=3;
-      int divide=a/b;
-         System.out.println(divide);
-}
-}
